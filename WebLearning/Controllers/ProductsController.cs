840160c32c689fd6ae877cfc5d22138776f8c0df
Bogdan Gilmanov <Bogdan_Gilmanov@rntgroup.com>
2022-12-23 19:17:37 +0400

    Added amountOfProducts configuration

--- WebLearning/Controllers/ProductsController.cs
+++ WebLearning/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using WebLearning.Contexts;
 
@@ -8,16 +9,30 @@ namespace WebLearning.Controllers
     public class ProductsController : Controller
     {
         private readonly NorthwindContext context;
+        private readonly IConfiguration configuration;
 
-        public ProductsController(NorthwindContext context)
+        public ProductsController(NorthwindContext context, IConfiguration configuration)
         {
             this.context = context;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            var products = context.Products.Include(x => x.Supplier).Include(x => x.Category).Take(5).ToList();
-            return View(products);
+            var products = context.Products
+                .Include(x => x.Supplier)
+                .Include(x => x.Category)
+                .AsQueryable();
+
+            var amountOfProductsShown = configuration["AmountOfProductsShown"];
+
+            if (int.TryParse(amountOfProductsShown, out var productCount) && 
+                productCount != 0)
+            {
+                products = products.Take(productCount);
+            }
+                
+            return View(products.ToList());
         }
     }
 }
